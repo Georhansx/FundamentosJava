dd57fe780cb46c23adddfbcb8edcc05c9ccd5ddc
Georhansx <georgeh.palomino@gmail.com>
2024-12-23 18:51:23 -0500

    Mi primer conditional

--- src/CondicionalIf.java
+++ src/CondicionalIf.java
@@ -1,9 +1,16 @@
-public class CondicionalIf {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CondicionalIf {
+
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
         //Condicional
-        int num1 = 3;
-        int num2= 5;
+        System.out.println("Digite um valor: ");
+        int num1 = sc.nextInt();
+        System.out.println("Digite um valor: ");
+        int num2= 5; sc.nextInt();
         int mult;
 
         if (num1 > num2){
